2e5fd01f0a617069dbd0dc4454d315b24bad6c15
Tessa Demel <254993102+tedemel@users.noreply.github.com>
2026-05-11 21:40:51 +0200

    ci: add trailing period to version.php Moodle comment

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026041503;
-$plugin->requires  = 2025041400;   // Moodle 5.0
+$plugin->requires  = 2025041400; // Moodle 5.0.
 $plugin->component = 'block_personalnotes';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
